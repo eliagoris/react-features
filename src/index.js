0d126178c9fc92715b2f1e6de92d88a07e1d38d9
Eduardo <eduardofidera@gmail.com>
2019-07-09 14:58:54 -0300

    add another state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = ({ initialCount }) => {
+const App = ({ initialCount, initialText }) => {
   const [count, setCount] = useState(initialCount)
+  const [text, setText] = useState(initialText)
 
   return (
     <div>
-      <p>The current count is {count}</p>
+      <p>
+        The current {text} is {count}
+      </p>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(0)}>reset</button>
       <button onClick={() => setCount(count + 1)}>+1</button>
+
+      <input value={text} onChange={e => setText(e.target.value)} />
     </div>
   )
 }
 
 App.defaultProps = {
   initialCount: 0,
+  initialText: 'count',
 }
 
 ReactDOM.render(<App initialCount={2} />, document.getElementById('root'))
